a56607839fe22114cb50a850252c8ea92a87e687
Navi <pixeldevs.diego@gmail.com>
2026-05-25 20:46:16 -0600

    Add single to SceneEventsSample.cs, Fix utilities typos.

--- SceneEvents/Samples~/SceneEventsSample.cs
+++ SceneEvents/Samples~/SceneEventsSample.cs
@@ -5,9 +5,21 @@ namespace PMG.Utilities.SceneEvents.Demo
 {
     public class SceneEventsSample : MonoBehaviour
     {
+        public static SceneEventsSample Instance { get; private set; }
+
         private void OnEnable() => SceneEvents.ActiveSceneChanged += Test;
         private void OnDisable() => SceneEvents.ActiveSceneChanged -= Test;
-        private void Awake() => DontDestroyOnLoad(gameObject);
+        private void Awake()
+        { 
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
 
         private void Test(string scene1Name, Scene scene2) => Debug.Log($"Scene changed from {scene1Name} to {scene2.name}");
     }
